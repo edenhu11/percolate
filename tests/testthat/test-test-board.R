6adbf7b2020b0bedc1a8e3399b50dacbb71c99ac
Eden Hu <edenhu11@gmail.com>
2019-08-02 14:29:36 -0400

    added board testing fns

---
+++ tests/testthat/test-test-board.R
@@ -0,0 +1,20 @@
+test_that("board() works", {
+  mat1 <- matrix(0, 5, 5)
+  mat2 <- board(mat1)
+  expect_equivalent(mat1, unclass(mat2) & attr(mat2, "n") == 5 & attr(mat2, "p") == 1)
+})
+
+test_that("board() errors for non-square matrix", {
+  expect_error(board(matrix(0, 1, 2)))
+})
+
+test_that("board() errors for containing numbers other 0, 1, 2", {
+  mat1 <- matrix(0, 3, 3)
+  mat1[4] <- 3
+  expect_error(board(mat1))
+})
+
+test_that("board() errors for containing strings", {
+  mat1 <- matrix("a", 3, 3)
+  expect_error(board(mat1))
+})
